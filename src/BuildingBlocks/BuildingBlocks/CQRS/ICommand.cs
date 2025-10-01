bfe430eeb3f1e14d79af1a3b46c62b09ac799e3e
Erlin Skenderi <erlinskenderi@gmail.com>
2025-10-01 17:14:16 -0500

    after adding marten

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,16 @@
+﻿
+using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+
+
+
+    public interface Icommand : ICollection<Unit>
+    {
+
+    }
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+    }
+}
